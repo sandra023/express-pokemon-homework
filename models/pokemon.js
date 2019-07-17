703802fb67914a222c79cbdc4f82c581551b20d4
Sandra Abernathy <sandraabernathy@Sandras-MacBook-Pro.local>
2019-07-16 19:01:10 -0600

    Commit 6 - set up serving of static files so we can add CSS

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,9 +1,10 @@
-const pokemon = [ {name: "Bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"},
-				{name: "Ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur.jpg"},
-				{name: "Venusaur", img: "http://img.pokemondb.net/artwork/venusaur.jpg"},
-				{name: "Charmander", img: "http://img.pokemondb.net/artwork/charmander.jpg"},
-				{name: "Charizard", img: "http://img.pokemondb.net/artwork/charizard.jpg"},
-				{name: "Squirtle", img: "http://img.pokemondb.net/artwork/squirtle.jpg"},
-				{name: "Wartortle", img: "http://img.pokemondb.net/artwork/wartortle.jpg"}
+const pokemon = [ {name: "Bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur.jpg", color: 'lightgreen'},
+				{name: "Ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur.jpg", color: "mediumseagreen"},
+				{name: "Venusaur", img: "http://img.pokemondb.net/artwork/venusaur.jpg", color: "teal" },
+				{name: "Charmander", img: "http://img.pokemondb.net/artwork/charmander.jpg", color: "orangered"},
+				{name: "Charizard", img: "http://img.pokemondb.net/artwork/charizard.jpg", color: "red"},
+				{name: "Squirtle", img: "http://img.pokemondb.net/artwork/squirtle.jpg", color: "lightblue"},
+				{name: "Wartortle", img: "http://img.pokemondb.net/artwork/wartortle.jpg", color: "darkcyan"}
 			  ];
 module.exports = pokemon;
+
